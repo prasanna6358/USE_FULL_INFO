14a75cf03cd4775ed7031de1629c9bff35467cb0
billaprasanna <prasannab@caspex.com>
2018-08-01 13:02:34 +0530

    typescripts

--- typescript2.ts
+++ typescript2.ts
@@ -1,23 +1,5 @@
 
-class Mouse{
-
-    constructor(private _x?: number, private _y?: number){
-    }
-
-    addElements(){
-        console.log("X is "+this._x +" Y is "+this._y+" sum is "+(this.x+this._y));
-    }
-
-    get x(){
-        return this._x;
-    }
-    set x(value){
-        if(value<0){
-            throw new Error("value cannot be less than Zero");
-        }
-        else this._x = value;
-    }
-}
+import { Mouse } from "./Mouse";
 
 let obj = new Mouse(2,5);
 obj.addElements();
